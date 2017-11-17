7b7de5f457b8d4440776d3587e7e10b2d1235cb0
apple <apple@apples-Mac-mini.local>
2017-11-17 18:18:59 +0530

    first commit

---
+++ Webservice/Model/CustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell.swift
+//  Webservice
+//
+//  Created by apple on 17/11/17.
+//  Copyright © 2017 apple. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
